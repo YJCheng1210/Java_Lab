843edb884c854a7f743853cc08448a8176fa9d93
Yan Jhong Cheng <yjcheng1210@gmail.com>
2022-07-31 16:32:43 +0800

    Create SwitchDemo.java

---
+++ SwitchDemo.java
@@ -0,0 +1,26 @@
+
+public class SwitchDemo {
+	public static void main(String[] args) {
+		System.out.println();
+		
+		switch (6) {
+			case 3:
+				System.out.println("The choice is 3...");
+				break;
+			case 4:
+				System.out.println("The choice is 4...");
+				break;
+			case 5:
+				System.out.println("The choice is 5...");
+				break;
+			case 6:
+				System.out.println("The choice is 6...");
+				break;
+			default:
+				System.out.println("No condition met...");			
+		}
+		
+		System.out.println();
+	}
+
+}
