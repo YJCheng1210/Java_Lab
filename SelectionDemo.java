acdf0f78f2877fe99e276a95e00d9541d747ad89
Yan Jhong Cheng <yjcheng1210@gmail.com>
2022-07-31 16:25:24 +0800

    Create SelectionDemo.java

---
+++ SelectionDemo.java
@@ -0,0 +1,23 @@
+
+public class SelectionDemo {
+	public static void main(String[] args) {
+		System.out.println();
+		
+		if (3 > 5) {
+			System.out.println("3 greater than 5 happened!");
+		}
+		else if (4 > 5) {
+			System.out.println("4 greater than 5 happened!");
+		}
+		else if (5 > 5) {
+			System.out.println("5 greater than 5 happened!");
+		}
+        else if (6 > 5) {
+            System.out.println("Of course 6 is greater than 5!");
+        }
+		else {
+			System.out.println("No conditions met.");
+		}
+		System.out.println();
+	}
+}
