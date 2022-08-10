c00c7fe65df80c27df8226d3e808eccd87265892
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-08-10 17:38:00 +0800

    Calculate the factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		// year to be checked
+		int num = scn.nextInt();		
+		long factorial = 1;
+		for(int i = 1; i <= num; ++i) {
+			// factorial = factorial * i;
+			factorial *= i;
+		}
+		System.out.printf("Factorial of %d = %d", num, factorial);
+	}
+}
