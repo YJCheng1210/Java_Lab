4d06dc896bc96c480fd39352927f7ce29f8dfd83
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-08-10 17:33:38 +0800

    Check if a leap year

---
+++ LeapYear.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		// year to be checked
+		int year = scn.nextInt();
+		boolean leap = false;
+		// if the year is divided by 4
+		if (year % 4 == 0) {
+			// if the year is century
+		    if (year % 100 == 0) {
+		    // if year is divided by 400
+		    // then it is a leap year
+		    	if (year % 400 == 0)
+		    		leap = true;
+		    	else
+		    		leap = false;
+		    }
+		    // if the year is not century
+		    else
+		    	leap = true;
+		    }
+		else
+			leap = false;
+		
+		if (leap)
+			System.out.println(year + " is a leap year.");
+		else
+			System.out.println(year + " is not a leap year.");
+	}
+}
